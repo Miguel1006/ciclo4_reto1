c4f8a0daaa6fb770c7993a39281a0b863c6f56c8
Miguel1006 <110136200+Miguel1006@users.noreply.github.com>
2022-11-08 12:23:23 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -246,7 +246,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             },
                           ),
                         );
-                      }))
+                      },),),
             ],
           )),
     );
